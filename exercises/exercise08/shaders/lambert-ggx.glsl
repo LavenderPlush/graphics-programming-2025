808e45d376d9cd710242e94569d618b104ce8da4
Olivier-Baptiste Hansen <oliha@itu.dk>
2026-03-23 16:00:19 +0100

    Exercise 8.7
    Co-authored-by: Benjamin Juul <juules32@gmail.com>

--- exercises/exercise08/shaders/lambert-ggx.glsl
+++ exercises/exercise08/shaders/lambert-ggx.glsl
@@ -23,17 +23,17 @@ vec3 GetAlbedo(SurfaceData data)
 	// (todo) 08.7: Adjust albedo with metalness
 
 
-	return data.albedo;
+	return mix(data.albedo, vec3(0.0f), data.metalness);
 }
 
 // Get the surface reflectance
 vec3 GetReflectance(SurfaceData data)
 {
 	// (todo) 08.7: Get reflectance value for metals
-
+    vec3 reflectance = mix(vec3(0.04f), data.albedo, data.metalness);
 
 	// We use a fixed value for dielectric, with a typical value for these materials (4%)
-	return vec3(0.04f);
+	return reflectance;
 }
 
 // Schlick simplification of the Fresnel term
